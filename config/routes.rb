0191a596eab0dca579c2974461f52781f2cc90b7
a_maulana <cintamupalsu@hotmail.com>
2024-04-02 10:36:29 +0900

    Finish common pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'common_pages#home'
   get 'common_pages/home'
   get 'common_pages/help'
-  root 'application#hello'
+  get 'common_pages/about'
+  get 'common_pages/contact'
+  #root 'application#hello'
 end
--- test/controllers/common_pages_controller_test.rb
+++ test/controllers/common_pages_controller_test.rb
@@ -1,13 +1,31 @@
 require "test_helper"
 
 class CommonPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "🐞Buburuza"
+  end
+
+  test "should get root" do 
+    get root_url
+    assert_response :success
+  end
+
   test "should get home" do
     get common_pages_home_url
     assert_response :success
+    assert_select "title", "Home | #{@base_title}"
   end
 
   test "should get help" do
     get common_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
   end
+
+  test "should get about" do
+    get common_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end 
 end
