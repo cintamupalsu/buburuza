eac3140d1a7292bfa529f1ff72858086e68de50f
a_maulana <cintamupalsu@hotmail.com>
2024-04-09 11:17:11 +0900

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'common_pages#home'
-  get 'common_pages/home'
-  get 'common_pages/help'
-  get 'common_pages/about'
-  get 'common_pages/contact'
-  #root 'application#hello'
+  get '/help',                to: 'common_pages#help'
+  get '/about',               to: 'common_pages#about'
+  get '/contact',             to: 'common_pages#contact'
+  get '/signup',              to: 'users#new'
 end
